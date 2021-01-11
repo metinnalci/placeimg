5913db71007a63e78a03715329be7ff57538bcb7
metinnalci@outlook.com <metinnalci@outlook.com>
2021-01-11 15:15:24 +0300

    Add project files.

---
+++ PlaceIMG.Business/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PlaceIMG.Business
+{
+    public class Class1
+    {
+    }
+}
---
+++ PlaceIMG.Data/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PlaceIMG.Data
+{
+    public class Class1
+    {
+    }
+}
