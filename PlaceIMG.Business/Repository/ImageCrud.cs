da8fe57012f5d8e1ce4cceb6f83a7a9b672340bd
Doğan Aker <m.doganaker@gmail.com>
2021-01-13 18:24:27 +0300

    Issue #5 ( Test ), Issue #9 ( Test ) aşaması

---
+++ PlaceIMG.Business/Repository/ImageCrud.cs
@@ -0,0 +1,18 @@
+﻿using PlaceIMG.Business.Interfaces;
+using PlaceIMG.Data.Models.Context;
+using PlaceIMG.Data.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlaceIMG.Business.Repository
+{
+    public class ImageCrud : IImageCrud
+    {
+        private readonly ImageContext context;
+        public void Add(Image image)
+        {
+            context.Set<Image>().Add(image);
+        }
+    }
+}
--- PlaceIMG.Business/Repository/ImageRepository.cs
+++ PlaceIMG.Business/Repository/ImageRepository.cs
@@ -1,7 +1,9 @@
 ﻿using PlaceIMG.Business.Interfaces;
+using PlaceIMG.Data.Models.Context;
 using PlaceIMG.Data.Models.Entities;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -9,14 +11,21 @@ namespace PlaceIMG.Business.Repository
 {
     public class ImageRepository : IImage
     {
+        private readonly ImageContext context;
+        public ImageRepository()
+        {
+            context = new ImageContext();
+        }
         public double GetRatio(double width, double height)
         {
-            throw new NotImplementedException();
+            var ratio = width / height;
+            return ratio;
         }
 
         public Image PickOne(Expression<Func<Image, bool>> predicate)
         {
-            throw new NotImplementedException();
+            var image = context.Set<Image>().FirstOrDefault(predicate);
+            return image;
         }
     }
 }
