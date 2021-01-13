e461d0418156fd2cf319f24630da7eb02e7e61ce
Doğan Aker <m.doganaker@gmail.com>
2021-01-13 10:32:18 +0300

    Issue #4 ( Test ), interface oluşturuldu

---
+++ PlaceIMG.Business/Repository/IImage.cs
@@ -0,0 +1,15 @@
+﻿using PlaceIMG.Data.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace PlaceIMG.Business.Repository
+{
+    public interface IImage
+    {
+        Image PickOne(Expression<Func<Image, bool>> predicate);
+        double GetRatio(double width, double height);
+
+    }
+}
