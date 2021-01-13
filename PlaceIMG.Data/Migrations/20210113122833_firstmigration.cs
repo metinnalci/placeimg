5023fd0d4ba357b4ae59c00d4b8e6b6ba1f153df
metinnalci@outlook.com <metinnalci@outlook.com>
2021-01-13 15:35:58 +0300

    PostgreSQL entegrasyonu

---
+++ PlaceIMG.Data/Migrations/20210113122833_firstmigration.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace PlaceIMG.Data.Migrations
+{
+    public partial class firstmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    CatName = table.Column<string>(type: "text", nullable: true),
+                    TopCatID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Images",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    ImgPath = table.Column<string>(type: "text", nullable: true),
+                    Ratio = table.Column<double>(type: "double precision", nullable: false),
+                    CatID = table.Column<int>(type: "integer", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Images", x => x.ID);
+                    table.ForeignKey(
+                        name: "FK_Images_Categories_CatID",
+                        column: x => x.CatID,
+                        principalTable: "Categories",
+                        principalColumn: "ID",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Images_CatID",
+                table: "Images",
+                column: "CatID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Images");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+        }
+    }
+}
--- PlaceIMG.Data/Models/Context/ImageContext.cs
+++ PlaceIMG.Data/Models/Context/ImageContext.cs
@@ -10,7 +10,8 @@ namespace PlaceIMG.Data.Models.Context
     {
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"serverparametreleri");
+            optionsBuilder.UseNpgsql(@"Server=ec2-54-195-247-108.eu-west-1.compute.amazonaws.com
+;Database=dav2rosrm03b0v;UID=lvzegvgmspykdd;PWD=d0ecee59ece60b79a6d70880690851d0fec8d0a794872d15cd2410d2751c0bf6;SSL Mode= Require;TrustServerCertificate=True");
         }
 
         public DbSet<Category> Categories { get; set; }
@@ -21,4 +22,3 @@ namespace PlaceIMG.Data.Models.Context
 
 
 
-//add-migration ve update-database yapılmalı...
